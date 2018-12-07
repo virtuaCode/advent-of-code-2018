00c49a12a9565a4e9e3a46ec85a457b8140da27a
virtuaCode <michael.schmidt.dev@gmail.com>
2018-12-07 20:02:26 +0100

    Add solution (part A) for day #7

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { day3 } from './days/day3';
 import { day4 } from './days/day4';
 import { day5 } from './days/day5';
 import { day6 } from './days/day6';
+import { day7 } from './days/day7';
 
 /**
  * Run `npm start <number>` to execute the solution for an specific day 
@@ -22,6 +23,7 @@ const solutions = [
   day4,
   day5,
   day6,
+  day7,
 ];
 
 if (Number.isNaN(day) || day >= solutions.length || day < 0) {
